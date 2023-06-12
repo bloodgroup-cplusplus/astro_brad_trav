147f27440bf06d775710857339fda7d1c27afb64
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-12 19:17:34 +0530

    i dont knwo

---
+++ public/src/data/navData.js
@@ -0,0 +1,11 @@
+const navData=[{
+    name:"About",
+    path:"/about",
+},
+{
+    name:"Packages",
+    path:"/packages"
+}
+
+]
+export default navData;
---
+++ src/js/nav.js
@@ -0,0 +1,7 @@
+const navLinks = document.querySelector("[data-navLink]");
+navLinks.forEach((link)=>{
+    if(link.getAttribute("href")=== window.location.pathname)
+    {
+        link.setAttribute("aria-current","page")
+    }
+})
